b45506fdc039896e9991f25eab3c6e3036a7964e
Palash1256 <palashmanna828@gmail.com>
2025-01-24 17:56:47 +0530

    dashboard completed

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client"
+import { UserButton } from '@clerk/nextjs'
+import React, { useState } from 'react';
+import EmptyState from './_components/EmptyState';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+//import { useState} from 'react';
 
 function Dashboard() {
+  const [videoList,setVideoList]=useState([]);
   return (
     <div>
-      Dashboard
+      <div className='flex justify-between item-center'>
+        <h2 className='font-bold text-2xl text-primary'> Dashboard </h2>
+        <Link href={'/dashboard/create-new'}>
+            <Button>+ Create new</Button>
+        </Link>
+      </div>
+      {/*Empty State*/}
+      {videoList?.length==0&&<div>
+        <EmptyState/>
+      </div>}
     </div>
   )
 }
